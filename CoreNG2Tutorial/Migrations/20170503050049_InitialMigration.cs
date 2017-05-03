76b410d1d28f72fb780e43b9ef1cef0fc0d17fd0
Magizhchi <karthikk296d@gmail.com>
2017-05-03 00:01:06 -0500

    Adding Migration for easy copy of the DB schema

---
+++ CoreNG2Tutorial/Migrations/20170503050049_InitialMigration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoreNG2Tutorial.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "operations",
+                columns: table => new
+                {
+                    operationID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    operationDesc = table.Column<string>(type: "varchar(50)", nullable: false),
+                    operationName = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Table_1", x => x.operationID);
+                    table.ForeignKey(
+                        name: "FK_operations_operations",
+                        column: x => x.operationID,
+                        principalTable: "operations",
+                        principalColumn: "operationID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "history",
+                columns: table => new
+                {
+                    historyID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    argument1 = table.Column<int>(nullable: true),
+                    argument2 = table.Column<int>(nullable: true),
+                    operationID = table.Column<int>(nullable: false),
+                    result = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_history", x => x.historyID);
+                    table.ForeignKey(
+                        name: "FK_history_operations_operationID",
+                        column: x => x.operationID,
+                        principalTable: "operations",
+                        principalColumn: "operationID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_history_operationID",
+                table: "history",
+                column: "operationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_operations_operationID",
+                table: "operations",
+                column: "operationID",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "history");
+
+            migrationBuilder.DropTable(
+                name: "operations");
+        }
+    }
+}
---
+++ CoreNG2Tutorial/Migrations/NG2TutorialContextModelSnapshot.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using CoreNG2Tutorial.Models;
+
+namespace CoreNG2Tutorial.Migrations
+{
+    [DbContext(typeof(NG2TutorialContext))]
+    partial class NG2TutorialContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("CoreNG2Tutorial.Models.History", b =>
+                {
+                    b.Property<int>("HistoryId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("historyID");
+
+                    b.Property<int?>("Argument1")
+                        .HasColumnName("argument1");
+
+                    b.Property<int?>("Argument2")
+                        .HasColumnName("argument2");
+
+                    b.Property<int>("OperationId")
+                        .HasColumnName("operationID");
+
+                    b.Property<int?>("Result")
+                        .HasColumnName("result");
+
+                    b.HasKey("HistoryId");
+
+                    b.HasIndex("OperationId");
+
+                    b.ToTable("history");
+                });
+
+            modelBuilder.Entity("CoreNG2Tutorial.Models.Operations", b =>
+                {
+                    b.Property<int>("OperationId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("operationID");
+
+                    b.Property<string>("OperationDesc")
+                        .IsRequired()
+                        .HasColumnName("operationDesc")
+                        .HasColumnType("varchar(50)");
+
+                    b.Property<string>("OperationName")
+                        .IsRequired()
+                        .HasColumnName("operationName")
+                        .HasColumnType("varchar(50)");
+
+                    b.HasKey("OperationId")
+                        .HasName("PK_Table_1");
+
+                    b.HasIndex("OperationId")
+                        .IsUnique();
+
+                    b.ToTable("operations");
+                });
+
+            modelBuilder.Entity("CoreNG2Tutorial.Models.History", b =>
+                {
+                    b.HasOne("CoreNG2Tutorial.Models.Operations", "Operation")
+                        .WithMany("History")
+                        .HasForeignKey("OperationId");
+                });
+
+            modelBuilder.Entity("CoreNG2Tutorial.Models.Operations", b =>
+                {
+                    b.HasOne("CoreNG2Tutorial.Models.Operations", "Operation")
+                        .WithOne("InverseOperation")
+                        .HasForeignKey("CoreNG2Tutorial.Models.Operations", "OperationId")
+                        .HasConstraintName("FK_operations_operations");
+                });
+        }
+    }
+}
